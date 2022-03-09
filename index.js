00e0b2bc1755da4f7b6197810a0e80b60caba29b
Franz-Art-L <francislandia@icloud.com>
2022-03-09 23:59:04 +0800

    started to code the js from generating random number random equation and append them on the ui il continue to code tomorrow for the timer part the game is now playable but not yet finished

--- index.js
+++ index.js
@@ -1,4 +1,47 @@
-// countdown timer
+$(document).ready(() => {
+    var currentRandomQuestion;
+    var timeRemaining = 10;
 
-var userInput = document.querySelector('input');
-userInput.addEventListener('keydown', startCountDown);
+    var generateRandomNumber = number => {
+        return Math.floor(Math.random() * number);
+    }
+
+    var generateRandomQuestion = () => {
+        let questionObj = {};
+
+        let firstNumber = generateRandomNumber(10);
+        let secondNumber = generateRandomNumber(10);
+
+        questionObj.result = firstNumber + secondNumber;
+        questionObj.randomEquation = String(firstNumber) + " + " + String(secondNumber) + " = " + ' ?';
+
+        return questionObj;
+    }
+
+    var renderNewRandomQuestion = () => {
+        currentRandomQuestion = generateRandomQuestion();
+        $('#questionDiv').text(currentRandomQuestion.randomEquation);
+    }
+
+    var checkUserAnswer = (userInput, result) => {
+        if (userInput === result) {
+            renderNewRandomQuestion();
+            $('#user-answer').val('');
+        }
+    }
+
+    $('#user-answer').on('keyup', function() {
+        checkUserAnswer(Number($(this).val()), currentRandomQuestion.result);
+    });
+
+    $('#user-answer').on('click', function() {
+        checkUserAnswer(Number($(this).val()), currentRandomQuestion.result);
+    });
+
+    renderNewRandomQuestion();
+
+});
+
+setInterval(() => {
+    console.log('1 sec passed');
+}, 1000)
